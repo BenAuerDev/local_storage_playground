e96b907d3b195c45232cfad3c819f5f41ead5950
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-03-01 16:16:56 +0100

    add homescreen

---
+++ lib/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:local_storage_playground/views/flashlist_list.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FlashlistList();
+  }
+}
